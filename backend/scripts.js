e36f19961a5aaf0da72bd0b235ba0946f64d1b9d
llaxmi <ll4xmi@gmail.com>
2024-10-26 23:15:23 +0545

    minimize data

--- backend/scripts.js
+++ backend/scripts.js
@@ -2,7 +2,7 @@ import fs from "fs";
 
 try {
   // Read and parse the original data from data.json
-  const data = JSON.parse(fs.readFileSync("./data2.json", "utf-8"));
+  const data = JSON.parse(fs.readFileSync("./dataa.json", "utf-8"));
 
   const newData = data.map((item) => {
     return {
@@ -12,15 +12,14 @@ try {
       small_logo_thumb_url: item.small_logo_thumb_url,
       website: item.website,
       all_locations: item.all_locations,
-      long_description: item.long_description,
       one_liner: item.one_liner,
     };
   });
 
   // Write the new data to data-min.json
-  fs.writeFileSync("./data.json", JSON.stringify(newData, null, 2));
+  fs.writeFileSync("./minData.json", JSON.stringify(newData, null, 2));
 
-  console.log("New data-min.json file created successfully!");
+  console.log("New .json file created successfully!");
 } catch (err) {
   console.error("Error processing the files:", err);
 }
